86a3a97819e4a873b55d76d70789bfa1f105df37
Juan14vc <jv0407159@gmail.com>
2026-03-05 21:05:31 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ import psycopg2
 import os
 
 def get_connection():
-    # REEMPLAZA esta URL con la "External Connection String" de tu Dashboard de Render
-    # Se ve algo como: postgresql://user:password@endpoint.render.com/dbname
-    url = "postgresql://surfflow_db_n8n1_user:WUIsvcU1076u2AdlN9GBghroraT2eOhN@dpg-d6kbd84r85hc739jfkt0-a.ohio-postgres.render.com/surfflow_db_n8n1" 
+    # PEGA AQUÍ la URL de Render (la que empieza con postgresql://)
+    url = "postgresql://surfflow_db_n8n1_user:WUIsvcU1076u2AdlN9GBghroraT2eOhN@dpg-d6kbd84r85hc739jfkt0-a.ohio-postgres.render.com/surfflow_db_n8n1"
     return psycopg2.connect(url)
